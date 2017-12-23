36f98ae95728ce289a96087ea240067852ea15b1
Suresh Hariharan <suresh@sureshx.com>
2017-12-22 21:33:08 -0600

    Fix single-noun matching and modifying

--- application.js
+++ application.js
@@ -15,14 +15,16 @@
 $(document).ready(function(){
   $("button").on( 'click', function(){
     var original_text = $( "#original_text").val();
-    var doc = nlp(orginal_text);
+    var doc = nlp(original_text);
     var nouns = doc.nouns().out('array');
     var verbs = doc.verbs().out('array');
     var modified_text = original_text;
-    var noun = nouns(Math.floor(Math.random()*nouns.length))
-    for (noun){
-      modified_text = modified_text.replace(noun, '<span style="color:red;font-size:200%">'.concat'(noun, "</span>"));}
-            $("#rendered").html(modified_text);
+    var noun_index = Math.floor(Math.random()*nouns.length);
+    console.log(noun_index);
+    var noun = nouns[noun_index];
+    console.log(noun);
+    modified_text = modified_text.replace(noun, '<span style="color:red;font-size:200%">'.concat(noun, "</span>"));
+    $("#rendered").html(modified_text);
 
     });
   });
